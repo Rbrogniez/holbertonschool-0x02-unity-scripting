d997ce4d2269d0907583bac36a62be2e5c3325c7
unknown <5013@holbertonschool.com>
2023-09-07 15:30:57 +0200

    Scoreboard

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float  forwardForce = 500f;
+    public float forwardForce = 500f;
     public float sideForce = 500f;
     private int score = 0;
     public int health = 5;
@@ -12,60 +13,82 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Vous pouvez ajouter des initialisations ici si nécessaire.
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("d"))
+        if (health <= 0)
         {
-            rb.AddForce(sideForce * Time.deltaTime,0,0);
-        }
+            // Si la santé du joueur atteint 0 ou moins, c'est le jeu terminé.
+            Debug.Log("Game Over!");
 
-        if(Input.GetKey("a"))
+            // Rechargez la scène actuelle pour redémarrer le jeu.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            // Réinitialisez la santé et le score aux valeurs d'origine.
+            health = 5;
+            score = 0;
+        }
+        else
         {
-            rb.AddForce(-sideForce * Time.deltaTime,0,0);
-        }
+            // Si la santé n'est pas à zéro, vous pouvez continuer à contrôler le joueur.
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+            }
 
-        if(Input.GetKey("w"))
-        {
-            rb.AddForce(0,0,forwardForce * Time.deltaTime);
-        }
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+            }
 
-        if(Input.GetKey("s"))
-        {
-            rb.AddForce(0,0,-forwardForce * Time.deltaTime);
-        }
+            if (Input.GetKey("w"))
+            {
+                rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+            }
 
+            if (Input.GetKey("s"))
+            {
+                rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-         // Check if the entered collider has the "Pickup" tag.
+        // Vérifiez si le collider entré a le tag "Pickup".
         if (other.CompareTag("Pickup"))
         {
-            // Increment the score.
+            // Incrémentez le score.
             score++;
 
-            // Print the new score to the console.
+            // Affichez le nouveau score dans la console.
             Debug.Log("Score: " + score);
 
-            // Disable or destroy the Coin object.
-            // You can choose either option depending on your game logic.
-            // For disabling:
+            // Désactivez ou détruisez l'objet "Coin".
+            // Vous pouvez choisir l'une ou l'autre option en fonction de votre logique de jeu.
+            // Pour la désactivation :
             other.gameObject.SetActive(false);
 
-            // For destroying:
+            // Pour la destruction :
             // Destroy(other.gameObject);
         }
 
-        if (other.CompareTag("Trap" ))
+        if (other.CompareTag("Trap"))
         {
+            // Réduisez la santé du joueur en cas de collision avec un "Trap".
             health--;
 
+            // Affichez la santé mise à jour dans la console.
             Debug.Log("Health: " + health);
+        }
 
+        if (other.CompareTag("Goal"))
+        {
+            // Le joueur a atteint son objectif.
+            Debug.Log("You Win !");
         }
     }
 }
